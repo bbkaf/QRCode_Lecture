b6d969c3ce2f185ec6452baf40407da0589d0ca7
Hank <bbkaf@hotmail.com>
2018-01-26 10:09:27 +0800

    lecture1

--- QRcode_L1/ViewController.swift
+++ QRcode_L1/ViewController.swift
@@ -8,18 +8,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    //MARK: - IBOutlet
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var constraintOfTableHeight: NSLayoutConstraint!
+    
+    //MARK: - Property
+    var name = "hank"
+    var number = 50
+    
+    
+    var userData = ["員工編號","員工姓名","聯絡電話","員工部門"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.delegate = self
+        myTableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        constraintOfTableHeight.constant = myTableView.contentSize.height
+    }
+    
+    
+//    override func viewWillLayoutSubviews() {
+//        super.viewWillLayoutSubviews()
+//        self.constraintOfTableHeight?.constant = self.myTableView.contentSize.height
+//    }
+
+    //MARK: - UITableViewDataSource delegate methode
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let number = userData.count
+        return number
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath)
+        let titleName = userData[indexPath.row]
+        cell.textLabel?.text = titleName
+        return cell
     }
 
-
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
